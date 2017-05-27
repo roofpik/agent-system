51de0ecbd0841ac80fb94b90a34dd13ef1c7a285
Digamber-Sharma <dgsharmad@gmail.com>
2017-05-27 14:42:04 +0530

    all pages responsive

--- app/resources/general/controllers/ownerdetailsCtrl.js
+++ app/resources/general/controllers/ownerdetailsCtrl.js
@@ -2,6 +2,14 @@ app.controller('ownerdetailsCtrl', function($scope, $state, $timeout) {
     // ga('send', 'home');
     $('.modal').modal('close');
      
+  $('.button-collapse1').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
+     
     $('.parallax').parallax();
     $timeout(function() {
         $('.carousel-slider').carousel({
